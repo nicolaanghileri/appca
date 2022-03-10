15270845936bfbe4744fb1710d7cf90692982870
nicolaanghileri <nicola.anghileri@samtrevano.ch>
2022-03-10 11:58:27 +0100

    Aggiunto tutti i file

---
+++ web-ca/src/pages/fishino.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Fishino = () => {
+    console.log('fishino');
+    return (
+        <div>
+            <h1>FISHINO</h1>
+        </div>
+    );
+};
+
+export default Fishino;

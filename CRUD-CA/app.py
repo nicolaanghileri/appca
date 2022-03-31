f8aff79459fadf46843b73d997ec3b82c3fd761a
nicolaanghileri <nicola.anghileri@samtrevano.ch>
2022-03-31 09:28:33 +0000

    modificata route fishino

--- CRUD-CA/app.py
+++ CRUD-CA/app.py
@@ -1,25 +1,11 @@
 from flask import Flask, render_template, redirect, url_for, request,session
 from flask_navigation import Navigation
 from flask_mysqldb import MySQL
-
-
 from mysql.connector import (connection)
-
+import logging
 import mysql.connector
-from mysql.connector import errorcode
 
-try:
-  cnx = mysql.connector.connect(user='root',password='',host='localhost',database='ca')
-except mysql.connector.Error as err:
-  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-    print("Something is wrong with your user name or password")
-  elif err.errno == errorcode.ER_BAD_DB_ERROR:
-    print("Database does not exist")
-  else:
-    print(err)
-else:
-    print("Test database connection worked!")
-    cnx.close()
+
 
 
 app = Flask(__name__)
@@ -27,12 +13,22 @@ app.secret_key = "lachiavepiusegretadelmondo"
 nav = Navigation(app)
 
 # MySQL configurations
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'root'
-app.config['MYSQL_DB'] = 'ca'
 
-mysql = MySQL(app)
+def get_connection():
+  try:
+    cnx = mysql.connector.connect(user='adminca',password='Password&1',host='10.20.5.38',database='ca')
+    return cnx
+  except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+      print("Something is wrong with your user name or password")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+      print("Database does not exist")
+    else:
+      print(err)
+  else:
+      print("Test database connection worked!")  
+  
+
 nav.Bar('navbar',[
     nav.Item('Home', 'index'),
     nav.Item('Fishino', 'fishino'),
@@ -58,6 +54,22 @@ def login():
 
 @app.route("/fishino")
 def fishino():
+    try:
+      cnx = get_connection()
+      cursor = cnx.cursor(dictionary=True)
+      cursor.execute("SELECT * FROM fishino")
+      out = []
+      
+      for row in cursor:
+        #fihsino.py[key] = cursor
+        out.append(row)
+      logging.debug(f'fishino records: {out}')
+      
+      cursor.close()
+      cnx.close()
+    except e as err:
+      logging.exception(e)
+        
     return render_template('fishino.html')
 
     
@@ -66,4 +78,5 @@ def navbar():
     return render_template('navbar.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.debug = True
+    app.run(host="10.20.5.40")
--- CRUD-CA/db_config.py
+++ CRUD-CA/db_config.py
@@ -4,8 +4,8 @@ from flaskext.mysql import MySQL
 mysql = MySQL()
 
 # MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_USER'] = 'adminca'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'Password&1'
 app.config['MYSQL_DATABASE_DB'] = 'ca'
 app.config['MYSQL_DATABASE_HOST'] = '10.20.5.38'
 
